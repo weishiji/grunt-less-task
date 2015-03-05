47c9642d954d80867ac048c36f3b569ea667e326
刘小光 <liuxiaoguang@zihua.com.cn>
2015-03-05 10:46:09 +0800

    add .gitignore

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+console.log(arguments)
+module.exports = function(grunt){
+	require('jit-grunt')(grunt);
+	grunt.initConfig({
+		less : {
+			development : {
+				options : {
+					compress : false 
+					,yuicompress : true
+					,optimization : 2
+				}
+				,files : {
+					'css/main.css' : 'less/main.less'
+				}
+			}
+		}
+		,watch : {
+			styles : {
+				files : ['less/**/*.less']
+				,tasks : ['less']
+				,options : {
+					nospawn : true
+				}
+			}
+		}
+	});
+
+	grunt.registerTask('default',['less','watch'])
+}
